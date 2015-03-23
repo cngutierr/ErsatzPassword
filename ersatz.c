32bc5ab4c09e86018a1f4b6061762303dfb66dbf
cngutierr <cngutierr@gmail.com>
2015-03-22 20:54:10 -0400

    added copyright notice at top of files

--- ersatz.c
+++ ersatz.c
@@ -1,8 +1,43 @@
+/*-
+ * All rights reserved.
+ * Copyright (c) 2002-2003 Networks Associates Technology, Inc.
+ * All rights reserved.
+ *
+ * Portions of this software was developed for the FreeBSD Project by
+ * ThinkSec AS and NAI Labs, the Security Research Division of Network
+ * Associates, Inc.  under DARPA/SPAWAR contract N66001-01-C-8035
+ * ("CBOSS"), as part of the DARPA CHATS research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include "ersatz.h"
 #include "ersatz_words.h"
 #include <time.h>
 #include <stdlib.h>
-
+#include <gperftools/profiler.h>
 #define b64_ntop __b64_ntop
 #define b64_pton __b64_pton
 
@@ -102,6 +137,7 @@ int py_hsm_hmac(char *input_str, char *out_hash)
 	PyObject *tmp;
 	if(pyFunc_hmac && PyCallable_Check(pyFunc_hmac))
 	{
+		//ProfilerStart("/tmp/hsm_hmac.log");
 		pyArgs_hmac = PyTuple_New(2);
         pyStr_input = PyString_FromString(input_str);
         pyInt_key_handler = PyInt_FromLong(KEY_HANDLER);
@@ -118,6 +154,7 @@ int py_hsm_hmac(char *input_str, char *out_hash)
 		char *buff = PyString_AsString(tmp);
 		strcpy(out_hash, buff);
 
+		//ProfilerStop();
 		return HSM_HMAC_OK;
 	}
 	else
